431c08dd3b736f00e319388f9f41ac168e9fe355
guygoren <ggore337@mtroyal.ca>
2024-03-25 10:41:59 -0600

    Login Page - kinda

---
+++ Assignment2/src/components/LoginBox.jsx
@@ -0,0 +1,23 @@
+
+export const LoginBox = () => {
+    return (
+      <div>
+        <h1>Login</h1>
+        <label>
+        Email: <input name="myInput" /><br />
+        Password: <input name="myInput" /> <br />
+      </label>
+        <button
+        onClick={() => {
+          console.log("hello")
+        }
+      }
+      >
+        Hello
+        </button>
+        <button>
+          Register
+        </button>
+      </div>
+    );
+    }
