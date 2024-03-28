f9822ccb1bf040071d21444cb8067e404555e5b4
EGadsby <77995195+EGadsby@users.noreply.github.com>
2024-03-28 12:06:58 -0600

    Using the APIs thats a pogchamp
    lets go gamers

--- Assignment2/src/components/RaceList.jsx
+++ Assignment2/src/components/RaceList.jsx
@@ -5,6 +5,7 @@ import { setRaces } from "../slices/racesSlice";
 
 export const RaceList = () => {
   const races = useSelector((state) => state.race.races);
+  const selectedYear = useSelector((state) => state.year.selectedYear);
   const dispatch = useDispatch();
 
   const makeRaceList = (races) => {
@@ -12,20 +13,36 @@ export const RaceList = () => {
   }
 
   useEffect( () => {
-    let url = "https://6be00c83030542cebd2e939f67186dd2.api.mockbin.io/";
+    let url = `https://four513assignment1evangadsby.onrender.com/api/races/season/${selectedYear}`;
     console.log("‘fetching ... here to check if I’ve gone infinite’");
     fetch (url)
     .then(response => response.json())
     .then(data => {makeRaceList(data)
     console.log(races)
   });
-}, [])
+}, [selectedYear])
     
-    return (
-     <ul>
-      {races.map((race, index) => (
-        <li key={index}>{race.name}</li>
-      ) )}
-     </ul>
-    );
-  }
+return (
+  <div>
+    Who is the best racist?
+    <h1>{selectedYear}</h1>
+    <table style={{ "width": "100%" }}>
+      <thead>
+        <tr>
+          <th style={{ "width": "5%", "border": "1px solid #696969" }}>ID</th>
+          <th style={{ "width": "50%", "border": "1px solid #696969" }}>Name</th>
+          <th style={{ "width": "30%", "border": "1px solid #696969" }}>&nbsp;</th>
+        </tr>
+      </thead>
+      <tbody>
+        {races.map((race, index) => (
+            <tr key={index}>
+              <td>{index}</td>
+              <td>{race.name}</td>
+              <td>Some buttons</td>
+            </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+)}
